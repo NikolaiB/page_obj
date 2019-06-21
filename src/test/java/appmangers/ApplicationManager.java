a84adcf6802572ad159957ae69d29c27ce503bf1
nikolai <boikonikolai.sf@gmail.com>
2019-06-21 23:10:55 +0300

    NavigationHelper was added

--- src/test/java/appmangers/ApplicationManager.java
+++ src/test/java/appmangers/ApplicationManager.java
@@ -9,17 +9,18 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
     private WebDriver driver;
 
-    private final NavigationHelper navigationHelper = new NavigationHelper();
+    private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
 
     public void init() {
         driver = new FirefoxDriver();
+        groupHelper = new GroupHelper(driver);
+        sessionHelper = new SessionHelper(driver);
+        navigationHelper = new NavigationHelper(driver);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://localhost/addressbook");
-        groupHelper = new GroupHelper(driver);
         sessionHelper.login("admin", "secret");
-        sessionHelper = new SessionHelper(driver);
     }
 
     public void logout() {
--- src/test/java/appmangers/NavigationHelper.java
+++ src/test/java/appmangers/NavigationHelper.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.WebDriver;
 public class NavigationHelper {
     private WebDriver driver;
 
+    public NavigationHelper(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
     public void gotoGroupPage() {
         driver.findElement(By.linkText("groups")).click();
     }
